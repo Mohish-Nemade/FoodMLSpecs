f24c8119626f8cc5301e63d30946e67caaa65bcb
syferak <akshaytate01@gmail.com>
2018-03-02 16:34:57 +0530

    Add files via upload

--- FoodMLSpecs/DecodeImage.py
+++ FoodMLSpecs/DecodeImage.py
@@ -6,6 +6,6 @@ from io import BytesIO
 def decode_image(food_image):
     getimage = base64.decodestring(food_image['encoded_image'])
     i = Image.open(BytesIO(getimage))
-    path = r'D:\tensorflow\abcd'
+    path = r'/home/ml/akshay/' + str(food_image['id'])
     i.save(path + food_image['image_type'])
     return path+food_image['image_type']
--- FoodMLSpecs/Tensorflow.py
+++ FoodMLSpecs/Tensorflow.py
@@ -2,13 +2,12 @@ import subprocess, sys
 import operator
 
 def image_into_tensorflow(directory):
-    print('dfsfdf')
-    p = subprocess.Popen( r'python D:\tensorflow\tensorflow-master\tensorflow-master\tensorflow\examples\label_image\label_image.py --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt --input_layer=Mul --output_layer=final_result --input_mean=128 --input_std=128 --image=D:\tensorflow\tensorflow-master\tensorflow-master\tensorflow\examples\label_image\flower_photos\daisy\21652746_cc379e0eea_m.jpg',shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    print("dddsas")
+    p = subprocess.Popen( r'python $HOME/tensorflow/tensorflow/examples/label_image/label_image.py --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt --input_layer=Mul --output_layer=final_result --input_mean=128 --input_std=128 --image=' + directory,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     result=[]
     get=[]
     while True:
         out = p.stderr.read(1)
+        print(out)
         get.append(p.stdout.read(1))
         res = "".join(get)
         if out == '' and p.poll() != None:
@@ -29,4 +28,13 @@ def output_process(res):
     for i in range(0,len(res)-1,2) :
         if float(res[i+1]) > 0.80:
             result[res[i]] = float(res[i+1])
+    for key in result:
+        result[key] = {
+            'calories': 10,
+            'total-fat':25,
+            'protein':30,
+            'calcium':40,
+        }
+    print(result)
     return result
+
