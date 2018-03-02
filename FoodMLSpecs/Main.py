57d5680206d5fd93524b7a95c28408ec88a7292b
Mohish-Nemade <36949114+Mohish-Nemade@users.noreply.github.com>
2018-03-02 13:06:34 +0530

    Add files via upload

--- FoodMLSpecs/Main.py
+++ FoodMLSpecs/Main.py
@@ -5,25 +5,6 @@ app = Flask(__name__)
 
 
 result=[]
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-
-@app.route('/tate/', methods=['GET'])
-def get_tasks():
-    return "hello tate"
 
 
 @app.route('/image/', methods=['POST'])
@@ -33,27 +14,17 @@ def upload_image():
     food_image = {
         'id': request.json['id'],
         'encoded_image': request.json['encoded_image'],
-        #'result_status': request.json('result_status'),
+        'result_status': request.json.get('result_status'),
         'image_type': request.json.get('image_type'),
         #'food_name': request.json('food_image'),
        # 'food_details': request.json('food_details')
     }
 
-    a=12
-    result1 = {
-        'id': 1,
-        'title': a,
-        'description': 1,
-        'done': False
-    }
+
     save_directory = DecodeImage.decode_image(food_image)
-    Tensorflow.image_into_tensdorflow(save_directory)
-    print("tatti")
-   # print(result)
-    print("tatti2")
-   # print(result)
-    #return jsonify({'food_image': food_image}),201
-    return jsonify({'result1': result1}), 201
+    food_output = Tensorflow.image_into_tensorflow(save_directory)
+    food_image['food_name'] = food_output
+    return jsonify({'food_image': food_image}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
--- FoodMLSpecs/Tensorflow.py
+++ FoodMLSpecs/Tensorflow.py
@@ -1,6 +1,7 @@
 import subprocess, sys
+import operator
 
-def image_into_tensdorflow(directory):
+def image_into_tensorflow(directory):
     print('dfsfdf')
     p = subprocess.Popen( r'python D:\tensorflow\tensorflow-master\tensorflow-master\tensorflow\examples\label_image\label_image.py --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt --input_layer=Mul --output_layer=final_result --input_mean=128 --input_std=128 --image=D:\tensorflow\tensorflow-master\tensorflow-master\tensorflow\examples\label_image\flower_photos\daisy\21652746_cc379e0eea_m.jpg',shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     print("dddsas")
@@ -9,12 +10,23 @@ def image_into_tensdorflow(directory):
     while True:
         out = p.stderr.read(1)
         get.append(p.stdout.read(1))
+        res = "".join(get)
         if out == '' and p.poll() != None:
             break
         if out != '':
             result.append(out)
-    print(get)
+    print(res)
+    res = res.split("\r")
+    print(res)
 
+    res = "".join(res).replace('\n',' ').split(" ")
+    print(res)
+    final_result = output_process(res)
+    return final_result
 
-
-image_into_tensdorflow("fdafd")
+def output_process(res):
+    result = {}
+    for i in range(0,len(res)-1,2) :
+        if float(res[i+1]) > 0.80:
+            result[res[i]] = float(res[i+1])
+    return result
